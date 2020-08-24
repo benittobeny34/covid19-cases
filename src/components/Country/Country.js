8522b06b2880403b2e7d946c29f89d6885894f3b
benittoraj <brendonbeni42@gmail.com>
2020-08-24 15:15:36 +0530

    image correction

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,7 +3,7 @@ import './country.css';
 const Country = ({stats}) => {
     return(
         <div className='country'>
-            <img src={"https://www.countryflags.io/${stats.CountryCode}/flat/64.png"} alt={stats.Country}></img>
+            <img src={'https://www.countryflags.io/${stats.CountryCode}/flat/64.png'} alt={stats.Country}></img>
             <h2>{stats.Country}</h2>
             <div className='describe'>
                 <p>{`Active : ${stats.Active}`}</p> 
