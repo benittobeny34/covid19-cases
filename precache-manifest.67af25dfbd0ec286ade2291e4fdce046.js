b419536eb8aec1c4f732774bc0f93dd57bc77a55
benittoraj <brendonbeni42@gmail.com>
2020-08-24 15:08:24 +0530

    Updates

---
+++ precache-manifest.67af25dfbd0ec286ade2291e4fdce046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b55153f04d7bf53293f2b12b9c5e1a",
+    "url": "/covid19-cases/index.html"
+  },
+  {
+    "revision": "874247da8ec77c247d87",
+    "url": "/covid19-cases/static/css/main.5f26ae4c.chunk.css"
+  },
+  {
+    "revision": "3ce2baab7c4587ffeb03",
+    "url": "/covid19-cases/static/js/2.21fc6bb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-cases/static/js/2.21fc6bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874247da8ec77c247d87",
+    "url": "/covid19-cases/static/js/main.578a9ef6.chunk.js"
+  },
+  {
+    "revision": "2494c88086afa83ac356",
+    "url": "/covid19-cases/static/js/runtime-main.5d8808de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-cases/static/media/logo.5d5d9eef.svg"
+  }
+]);
